6b0d456983468286e0db913ac83a19b2c5feb764
Andres Orozco <juanxxi2015@gmail.com>
2022-11-06 17:41:52 -0500

    Upload the project into Github for deploy

---
+++ Models/TransactionDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDApplication.Models
+{
+    public class TransactionDBContext:DbContext
+    {
+        /** Esto sera usado para la inyeccion de dependencias en un futuro*/
+        public TransactionDBContext(DbContextOptions<TransactionDBContext>options):base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
